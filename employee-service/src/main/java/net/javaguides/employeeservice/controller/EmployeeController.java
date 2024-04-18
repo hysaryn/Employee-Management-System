bea7c13946cc3e4e876abaf8df6b04241d1dd924
yolandahe <he.yang7@northeastern.edu>
2024-04-17 17:45:52 -0700

    set up employee service and department service

---
+++ employee-service/src/main/java/net/javaguides/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package net.javaguides.employeeservice.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.employeeservice.dto.APIResponseDto;
+import net.javaguides.employeeservice.dto.EmployeeDto;
+import net.javaguides.employeeservice.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+  private EmployeeService employeeService;
+
+  @PostMapping
+  public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto) {
+    EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto);
+    return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+  }
+
+  @GetMapping("{id}")
+  public ResponseEntity<APIResponseDto> getEmployee(@PathVariable("id") Long employeeId) {
+    APIResponseDto apiResponseDto = employeeService.getEmployeeById(employeeId);
+    return new ResponseEntity<>(apiResponseDto, HttpStatus.OK);
+  }
+
+}
