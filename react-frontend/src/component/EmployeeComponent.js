4a4a6dd1c1e66af1860ae76ed8d266513eeab1d3
yolandahe <he.yang7@northeastern.edu>
2024-04-27 11:44:01 -0700

    design the page to show employee, department and organization details

--- react-frontend/src/component/EmployeeComponent.js
+++ react-frontend/src/component/EmployeeComponent.js
@@ -27,7 +27,47 @@ export default class EmployeeComponent extends Component {
   render() {
     return (
       <div>
-        
+        <div className="card col-mid-6 offset-md-3">
+            <h3 className="text-center card-header">View Employee Details</h3>
+            <div className='card-body'>
+                <div className="row">
+                    <p>Employee First Name: {this.state.employee.firstName}</p>
+                </div>
+                <div className="row">
+                    <p>Employee Last Name: {this.state.employee.lastName}</p>
+                </div>
+                <div className="row">
+                    <p>Employee Email: {this.state.employee.email}</p>
+                </div>
+            </div>
+
+            <h3 className="text-center card-header">View Department Details</h3>
+            <div className='card-body'>
+                <div className="row">
+                    <p>Department Name: {this.state.department.departmentame}</p>
+                </div>
+                <div className="row">
+                    <p>Department Description: {this.state.department.departmentDescription}</p>
+                </div>
+                <div className="row">
+                    <p>Department Code: {this.state.department.departmentCode}</p>
+                </div>
+            </div>
+
+            <h3 className="text-center card-header">View Organization Details</h3>
+            <div className='card-body'>
+                <div className="row">
+                    <p>Organization Name: {this.state.organization.organizationName}</p>
+                </div>
+                <div className="row">
+                    <p>Organization Description: {this.state.organization.organizationDescription}</p>
+                </div>
+                <div className="row">
+                    <p>Organization Code: {this.state.organization.organizationCode}</p>
+                </div>
+            </div>
+        </div>
+
       </div>
     )
   }
